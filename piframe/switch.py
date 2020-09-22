16f080fe4b08d49bddd1236c7e9646553caa4ce7
kbsezginel <kbsezginel@othot.com>
2020-09-22 00:59:55 -0400

    fix switch

--- piframe/switch.py
+++ piframe/switch.py
@@ -10,6 +10,9 @@ class Switch:
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         self.read()
 
+    def __repr__(self):
+        return "<Switch (pin: %i) is %s>" % (self.pin, self.state)
+
     def read(self):
         self.on = GPIO.input(self.pin)
         self.state = {True: "on", False: "off"}[self.on]
