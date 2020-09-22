212b590850a9eec772d6384bed9ebb013399d0ff
kbsezginel <kbsezginel@othot.com>
2020-09-22 00:22:41 -0400

    Toggle display fix

--- piframe/display.py
+++ piframe/display.py
@@ -13,21 +13,21 @@ class Display:
     def toggle(self, state, check_state=True):
         cmd = [self.exec, "display_power", {"on": "1", "off": "0"}[state]]
         if self.state != state or not check_state:
-            self.call(cmd)
+            self.run(cmd)
             self.state = state
         elif check_state:
             print(f"Display is already {self.state}, cannot turn {state}")
 
     def display_power(self):
         cmd = [self.exec, "display_power"]
-        self.call(cmd)
+        self.run(cmd)
 
     def measure_temp(self):
         cmd = [self.exec, "measure_temp"]
-        self.call(cmd)
+        self.run(cmd)
 
-    def call(self, cmd, out=True, err=False, verbose=True):
-        process = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def run(self, cmd, out=True, err=False, verbose=True):
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.stdout.decode(), process.stderr.decode()
         text = ""
         if out:
